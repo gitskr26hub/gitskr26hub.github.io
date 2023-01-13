8d4a03b4e36e0e8f2337c8cad834b0234c6599fa
skr <kumarsugam2609@gmail.com>
2023-01-13 17:53:42 +0530

    1/13/2023

--- src/components/About.js
+++ src/components/About.js
@@ -24,8 +24,10 @@ function About() {
         <Box className='stackbox'  >
         <Box  className="textbox" w={['100%','100%','70%']}>
 <Text fontSize={["2xl","3xl","5xl"]}  fontWeight="semibold">Hi, I am</Text>
-<Text fontSize={["3xl","4xl","6xl"]} fontWeight="bold" bgGradient="linear(to-r, cyan.300, blue.500, purple.700)" bgClip='text' >Sugam Kumar</Text>
-<Text fontSize={["3xl","4xl","6xl"]} fontWeight="bold" bgGradient="linear(to-r,purple.500, blue.400, cyan.400, )" bgClip='text' >Full Stack Web Developer</Text>
+<Text fontSize={["3xl","4xl","6xl"]} fontWeight="bold" bgGradient="linear(to-r, cyan.300, blue.500, purple.700)" 
+bgClip='text' >Sugam Kumar</Text>
+<Text fontSize={["3xl","4xl","6xl"]} fontWeight="bold" bgGradient="linear(to-r,purple.500, blue.400, cyan.400, )" 
+bgClip='text' >Full Stack Web Developer</Text>
 <Text fontSize={["14px","14px","20px"]} style={{textAlign:"justify",marginBottom:"2%"}} width={["100%","100%","100%"]} 
 color={isDark ? "white" : "#454545"}>
 Self-Motivated and Aspiring Full Stack Web Developer with expertise in creating user-friendly designs & architecture, and ready-to-use websites. Looking for an entry-level position in the web development field to utilize knowledge, improve skills, and for growth.</Text>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -22,10 +22,10 @@ const Contact = () => {
   <Box className='contact-detail'>
   <Box w={['60%',"40%",'30%']} style={{display:"grid",
   gridTemplateColumns:"repeat(2,1fr)",gap:"30px",margin:"3%"}}  >
-  <a href='https://www.linkedin.com/in/sugam-kumar-6b07041bb'>
+  <a href='https://www.linkedin.com/in/sugam-kumar-6b07041bb' target="_blank">
   <img src='https://1000logos.net/wp-content/uploads/2017/03/Linkedin-Logo.png'  /></a>
 
-  <a href='https://github.com/gitskr26hub'>
+  <a href='https://github.com/gitskr26hub' target="_blank">
   <img src='https://logos-world.net/wp-content/uploads/2020/11/GitHub-Emblem.png'/></a>
 
       </Box>
@@ -34,18 +34,24 @@ const Contact = () => {
       <Box 
       style={{display:"flex",alignItems:"center",height:"20px",margin:"3%",textAlign:"left",gap:"2%",}}>
       <PermPhoneMsgIcon/> 
-      <Heading fontSize={["15px",'20px','25px']} size={['sm','md','lg']} >Call me:+918299227112</Heading> </Box> 
+      <Heading fontSize={["15px",'20px','25px']} size={['sm','md','lg']} >
+      <a href="tel:+918299227112">
+        Call me:+918299227112</a></Heading> </Box> 
 
       <Box style={{display:"flex",alignItems:"center",height:"20px",margin:"3%",textAlign:"left",gap:"2%",}}>
       <EmailIcon/> 
       <Heading fontSize={["10px",'20px','25px']} size={['xs','md','lg']} >
       <Link  _hover={{background: "white",color: "blue.500",
-    }} type='email' href='mailto:kumarsugam26@gmail.com' isExternal>  Email:kumarsugam26@gmail.com</Link>
+    }} type='email' href='mailto:kumarsugam26@gmail.com' isExternal  >  Email:kumarsugam26@gmail.com</Link>
       </Heading> </Box> 
 
       <Box style={{display:"flex",alignItems:"center",height:"20px",margin:"3%",textAlign:"left",gap:"2%",}}>
       <FmdGoodIcon/> 
-      <Heading fontSize={["10px",'20px','25px']} size={['sm','md','lg']} className="location" >location: UTTAR PRADESH , INDIA 
+      <Heading fontSize={["10px",'20px','25px']} size={['sm','md','lg']} className="location" >
+        <a href="https://www.google.com/maps/place/Lucknow,+Uttar+Pradesh/@26.848623,80.8024261,11z/data=!3m1!4b1!4m5!3m4!1s0x399bfd991f32b16b:0x93ccba8909978be7!8m2!3d26.8466937!4d80.946166" target="_blank">
+        location: UTTAR PRADESH , INDIA 
+        </a>
+       
       </Heading> </Box> 
 
       </Box>
